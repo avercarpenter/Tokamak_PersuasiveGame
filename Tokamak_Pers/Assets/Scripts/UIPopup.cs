dab80380770c67411150c69d1a327154ddcc7c2f
Avery Carpenter <avercarpenter@gmail.com>
2023-04-25 16:02:13 -0400

    rain updates

---
+++ Tokamak_Pers/Assets/Scripts/UIPopup.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPopup : MonoBehaviour
+{
+    public GameObject uiCanvas; // reference to the UI canvas object
+    public string triggerTag = "Player"; // the tag of the object that can trigger the UI canvas
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // check if the object that entered the trigger has the correct tag
+        if (other.CompareTag(triggerTag))
+        {
+            // pause the game
+            Time.timeScale = 0f;
+
+            // activate the UI canvas
+            uiCanvas.SetActive(true);
+        }
+    }
+
+    public void OnContinueButtonClicked()
+    {
+        // deactivate the UI canvas
+        uiCanvas.SetActive(false);
+
+        // resume the game
+        Time.timeScale = 1f;
+    }
+}
